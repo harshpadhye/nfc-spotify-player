983c52288283ac2803e0c942e6b20179322ee082
Harsh Padhye <hp9rup@virginia.edu>
2020-12-19 18:44:52 -0500

    added web framework with flask

--- player.py
+++ player.py
@@ -7,26 +7,39 @@ To be integrated with NFC tags and iOS automation.
 
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+from flask import Flask, request
+import gunicorn
 import os
 import util
 
-# required scopes
-scope = "user-modify-playback-state playlist-read-private app-remote-control streaming"
+app = Flask(__name__)
 
-# authorize the client
-client = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
-# dictionary mapping playlist names to playlist uris (str:str)
-playlist_map = {}
+@app.route("/")
+def main():
 
-playlists = client.current_user_playlists()["items"]
-for pl in playlists:
-    # maps the cleaned playlist name to the uri
-    playlist_map[util.clean_string(pl["name"])] = pl["uri"]
+    # required scopes
+    scope = "user-modify-playback-state playlist-read-private app-remote-control streaming"
 
-to_play = util.clean_string(input("What Playlist?\t"))
+    # authorize the client
+    client = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
-# shuffles and starts playback of selected playlist
-client.shuffle(state=True)
-client.start_playback(
-    device_id=os.environ["MY_IPHONE_ID"], context_uri=playlist_map[to_play])
+    # dictionary mapping playlist names to playlist uris (str:str)
+    playlist_map = {}
+
+    playlists = client.current_user_playlists()["items"]
+    for pl in playlists:
+        # maps the cleaned playlist name to the uri
+        playlist_map[util.clean_string(pl["name"])] = pl["uri"]
+
+    # grabs the desired album to play from the url query parameters
+    to_play = util.clean_string(request.args.get("name", None))
+
+    # shuffles and starts playback of selected playlist
+    client.shuffle(state=True)
+    client.start_playback(
+        device_id=os.environ["MY_IPHONE_ID"], context_uri=playlist_map[to_play])
+
+
+if __name__ == "__main__":
+    app.run()
